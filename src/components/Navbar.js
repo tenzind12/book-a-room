b33d20361b13ef4f8dfbb802727818896889457e
Tenzin Dekyong <dhontenzin@gmail.com>
2022-05-24 22:36:49 +0200

    front end first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            HOTEL REACT
+          </a>
+          <button
+            className="navbar-toggler bg-light"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/register">
+                  Register
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/login">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
