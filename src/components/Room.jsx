b35f5fd0e321b884f45c0ab32f603dc6fd32b8a2
Tenzin Dekyong <dhontenzin@gmail.com>
2022-05-26 14:29:29 +0200

    modal viewDetails modal

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,4 +1,12 @@
+import { useState } from 'react';
+import { Button, Modal, Carousel } from 'react-bootstrap';
+
 export default function Room({ room }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="row shadow mt-3 rounded p-2">
       <div className="col-lg-4">
@@ -17,9 +25,34 @@ export default function Room({ room }) {
         </p>
 
         <div className="float-end">
-          <button className="btn btn-sm btn-dark">View Details</button>
+          <button className="btn btn-sm btn-dark" onClick={handleShow}>
+            View Details
+          </button>
         </div>
       </div>
+
+      {/* modal react-bootstrap */}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{room.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Carousel>
+            {room.imageurls.map((imageUrl, index) => (
+              <Carousel.Item key={index}>
+                <img className="d-block w-100" src={imageUrl} alt={room.name + index} />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+          <p>{room.description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
